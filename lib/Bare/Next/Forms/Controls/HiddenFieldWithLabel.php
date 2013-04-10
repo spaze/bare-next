834ec91f220a710fcb770d8dd5cad40a2a8bf319
Michal Špaček <mail@michalspacek.cz>
2013-04-10 18:18:36 +0200

    Add new form component used when you need to display a text only instead of select field with one value. It's like hidden field but with label and optional text.

---
+++ lib/Bare/Next/Forms/Controls/HiddenFieldWithLabel.php
@@ -0,0 +1,44 @@
+<?php
+namespace Bare\Next\Forms\Controls;
+
+/**
+ * Hidden form control used to store a non-displayed value but with label and optional text
+ *
+ * @author     Michal Špaček
+ */
+class HiddenFieldWithLabel extends \Nette\Forms\Controls\BaseControl
+{
+
+	/** @param  Html|string field text */
+	protected $text;
+
+
+	public function __construct($label = null, $value = null, $text = null)
+	{
+		parent::__construct($label);
+		$this->control->type = 'hidden';
+		$this->value = $value;
+		$this->text = $text;
+	}
+
+
+	/**
+	 * Generates control's HTML element.
+	 * @return Nette\Utils\Html
+	 */
+	public function getControl()
+	{
+		$input = parent::getControl()
+			->value($this->value === null ? '' : $this->value)
+			->data('nette-rules', null);
+
+		$container = \Nette\Utils\Html::el();
+		if ($this->text !== null) {
+			$container->add($this->text);
+		}
+		$container->add($input);
+		return $container;
+	}
+
+
+}

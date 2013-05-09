f63a33e7763acd862073490049fd87f33d1bd7c1
Michal Špaček <mail@michalspacek.cz>
2013-05-09 20:06:22 +0200

    Update with my changes: - namespaces - quote never/always/smart(when needed) - initial data (think UTF-8 BOM) - configurable newlines

--- lib/Bare/Next/Application/Responses/CsvResponse.php
+++ lib/Bare/Next/Application/Responses/CsvResponse.php
@@ -1,17 +1,32 @@
 <?php
+namespace Bare\Next\Application\Responses;
+
+
 /**
  * CSV download response.
  *
  * @author     Petr 'PePa' Pavel
+ * @author     Michal Špaček
+ * Based on NCsvResponse by Petr 'PePa' Pavel http://addons.nette.org/cs/csvresponse
  *
  * @property-read array  $data
  * @property-read string $name
  * @property-read bool   $addHeading
  * @property-read string $glue
  * @property-read string $contentType
- * @package Nette\Application\Responses
  */
-class NCsvResponse extends NObject implements IPresenterResponse {
+class CsvResponse extends \Nette\Object implements \Nette\Application\IResponse
+{
+
+	/** Never add quotes */
+	const QUOTES_NEVER = 0;
+
+	/** Always add quotes */
+	const QUOTES_ALWAYS = 1;
+
+	/** Add quotes only when needed */
+	const QUOTES_SMART = 2;
+
 	/** @var array */
 	private $data;
 
@@ -25,61 +40,71 @@ class NCsvResponse extends NObject implements IPresenterResponse {
 	public $glue;
 
 	/** @var string */
+	public $newLine;
+
+	/** @var integer */
+	public $addQuotes;
+
+	/** @var string */
+	private $initialData;
+
+	/** @var string */
 	private $charset;
 
 	/** @var string */
 	private $contentType;
+
 
 	/**
 	 * @param  string  data (array of arrays - rows/columns)
 	 * @param  string  imposed file name
 	 * @param  bool    return array keys as the first row (column headings)
 	 * @param  string  glue between columns (comma or a semi-colon)
+	 * @param  string  string to use as new line
+	 * @param  integer whether to add quotes
+	 * @param  string  data to start output with, think UTF-8 BOM
 	 * @param  string  MIME content type
 	 */
-	public function __construct($data, $name = NULL, $addHeading = TRUE, $glue = ',', $charset = NULL, $contentType = NULL) {
-		// ----------------------------------------------------
-		$this->data = $data;
-		$this->name = $name;
-		$this->addHeading = $addHeading;
-		$this->glue = $glue;
-		$this->charset = $charset;
-//     $this->charset = $charset ? $charset : 'UTF-8';
+	public function __construct($data, $name = null, $addHeading = true, $glue = ',', $newLine = "\r\n", $addQuotes = self::QUOTES_SMART, $initialData = '', $charset = null, $contentType = null)
+	{
+		$this->data        = $data;
+		$this->name        = $name;
+		$this->addHeading  = $addHeading;
+		$this->glue        = $glue;
+		$this->newLine     = $newLine;
+		$this->addQuotes   = $addQuotes;
+		$this->initialData = $initialData;
+		$this->charset     = $charset;
 		$this->contentType = $contentType ? $contentType : 'text/csv';
 	}
-
-
 
 
 	/**
 	 * Returns the file name.
 	 * @return string
 	 */
-	final public function getName() {
-		// ----------------------------------------------------
+	final public function getName()
+	{
 		return $this->name;
 	}
-
 
 
 	/**
 	 * Returns the MIME content type of a downloaded content.
 	 * @return string
 	 */
-	final public function getContentType() {
-		// ----------------------------------------------------
+	final public function getContentType()
+	{
 		return $this->contentType;
 	}
 
-
-	
 
 	/**
 	 * Sends response to output.
 	 * @return void
 	 */
-	public function send(IHttpRequest $httpRequest, IHttpResponse $httpResponse) {
-		// ----------------------------------------------------
+	public function send(\Nette\Http\IRequest $httpRequest, \Nette\Http\IResponse $httpResponse)
+	{
 		$httpResponse->setContentType($this->contentType, $this->charset);
 		
 		if (empty($this->name)) {
@@ -91,48 +116,61 @@ class NCsvResponse extends NObject implements IPresenterResponse {
 		$data = $this->formatCsv();
 		
 		$httpResponse->setHeader('Content-Length', strlen($data));
-		print $data;
+		echo $data;
 	}
 
-	
-	
-	
-	public function formatCsv() {
-		// ----------------------------------------------------
+
+	protected function formatCsv()
+	{
 		if (empty($this->data)) {
 			return '';
 		}
 	
-		$csv = array();
+		$csv = $this->initialData;
 		
 		if (!is_array($this->data)) {
 			$this->data = iterator_to_array($this->data);
 		}
-		$firstRow = reset($this->data);
 	
 		if ($this->addHeading) {
+			$firstRow = reset($this->data);
 			if (!is_array($firstRow)) {
 				$firstRow = iterator_to_array($firstRow);
 			}
 			
 			$labels = array();
 			foreach (array_keys($firstRow) as $key) {
-				$labels[] = ucwords(str_replace("_", ' ', $key));
+				$labels[] = ucwords(str_replace('_', ' ', $key));
 			} 
-			$csv[] = '"'.join('"'.$this->glue.'"', $labels).'"';
+			$csv .= $this->formatRow($labels);
 		}
 	
 		foreach ($this->data as $row) {
 			if (!is_array($row)) {
 				$row = iterator_to_array($row);
 			}
-			foreach ($row as $key => $value) {
-				$value = preg_replace('/[\r\n]+/', ' ', $value);  // remove line endings
-				$value = str_replace('"', '""', $value);          // escape double quotes
-			}
-			$csv[] = '"'.join('"'.$this->glue.'"', $row).'"';
+			$csv .= $this->formatRow($row);
 		}
 	
-		return join(PHP_EOL, $csv);
+		return $csv;
 	}
+
+
+	protected function formatRow($row)
+	{
+		foreach ($row as $key => &$value) {
+			$value = preg_replace('/[\r\n]+/', ' ', $value);
+			if ($this->addQuotes == self::QUOTES_ALWAYS) {
+				$value = '"' . $value . '"';
+			} elseif ($this->addQuotes == self::QUOTES_SMART) {
+				if (strpos($value,	'"') !== false) {
+					$value = str_replace('"', '""', $value);
+					$value = '"' . $value . '"';
+				}
+			}
+		}
+		return implode($this->glue, $row) . $this->newLine;
+	}
+
+
 }
